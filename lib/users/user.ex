64fec6145b148b9a6d698e87a5a5bb86dcdcaa0a
Matheus Pedroni <mpedroni77@gmail.com>
2021-12-28 14:21:55 -0300

    :tada: entrega do desafio

--- lib/users/user.ex
+++ lib/users/user.ex
@@ -3,7 +3,19 @@ defmodule Flightex.Users.User do
   @enforce_keys @keys
   defstruct @keys
 
-  def build do
-    # TO DO
+  def build(_name, _email, cpf) when is_integer(cpf), do: {:error, "Cpf must be a String"}
+
+  def build(name, email, cpf) do
+    uuid = UUID.uuid4()
+
+    {
+      :ok,
+      %__MODULE__{
+        id: uuid,
+        name: name,
+        email: email,
+        cpf: cpf
+      }
+    }
   end
 end
